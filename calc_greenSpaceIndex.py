a250c8efb201cd09c6340ea26ed90734cac5e460
cMarzal <cmr.maro@gmail.com>
2020-10-13 12:39:15 +0200

    Updated to save files in shp form in out folder + comments.

--- calc_greenSpaceIndex.py
+++ calc_greenSpaceIndex.py
@@ -1,18 +1,21 @@
 import pandas as pd
+import geopandas
 from read_shapeFile import loadShapeFile
 import numpy as np
 import time
 
+# Open censo xls file and remove unwanted rows
 columns = pd.read_excel('data/secciones_censales/2801_CensoMadrid2019.xls').loc[5]
 census = pd.read_excel('data/secciones_censales/2801_CensoMadrid2019.xls', names = columns, header = 8, index_col=0)
 census.index = census.index.str.strip()
 census = census.groupby(census.index).sum()
 
+# Combine the census population data with the ShapeFile of seccionesCensales
 seccionesCensales_shp = loadShapeFile('data/secciones_censales/SECC_CE_20200101_MADRID.shp')
-
 census_inhabitants = census.rename({'Total': 'Inhabitants'}, axis=1)['Inhabitants']
 census_inhabitants = seccionesCensales_shp.join(census_inhabitants, on='CUSEC')
 
+# Load shapefiles of green and constructed areas
 urbanGardens_shp = loadShapeFile('data/Carto_1000/11_HUERTO_URBANO_P.shp')
 gardenAreas_shp = loadShapeFile('data/Carto_1000/11_ZONA_AJARDINADA_P.shp')
 gardenInPatios_shp = loadShapeFile('data/Carto_1000/11_ZONA_AJARDINADA_SOBRE_PATIO_P.shp')
@@ -20,19 +23,25 @@ buildings_shp = loadShapeFile('data/Carto_1000/03_EDIFICIO_EN_CONSTRUCCION_P.shp
 buildingsUnderConstruction_shp = loadShapeFile('data/Carto_1000/03_EDIFICIO_INDEFINIDO_P.shp')
 undefinedBuildings_shp = loadShapeFile('data/Carto_1000/03_EDIFICIO_P.shp')
 
+# Crete arrays for the different calculated index
 greenSpaceIndex = []
 greenSpaceDensity = []
 greenSpaceBuiltSpaceRatio = []
 tic = time.clock()
+
+# Loop to calcaulate the different indexes for each census area
 for i in range(len(census_inhabitants.index)):
+       # Calcualte process time
        if((i+1)%100==0):
               toc = time.clock()
               print('Done with ' + str(i+1) + '/' + str(len(census_inhabitants.index)) + ' census. Time in this cicle: ' + str(toc-tic))
               tic = time.clock()
+
        seccionesCensal_polygon = census_inhabitants['geometry'][i]
        green_space = 0
        built_space = 0
 
+       # Calculate the area of green and build space for each census
        for urbanGarden_polygon in urbanGardens_shp['geometry']:
               if urbanGarden_polygon.intersects(seccionesCensal_polygon):
                      green_space += urbanGarden_polygon.intersection(seccionesCensal_polygon).area
@@ -58,6 +67,8 @@ for i in range(len(census_inhabitants.index)):
               if undefinedBuilding_polygon.intersects(seccionesCensal_polygon):
                      built_space += undefinedBuilding_polygon.intersection(seccionesCensal_polygon).area
 
+
+       # Make the calculations for each index, if there is any division by 0, 9999 will be added instead of the division
        try:
               greenSpaceIndex.append(green_space/census_inhabitants['Inhabitants'][i])
        except ZeroDivisionError:
@@ -73,10 +84,13 @@ for i in range(len(census_inhabitants.index)):
        except ZeroDivisionError:
               greenSpaceBuiltSpaceRatio.append(9999)
 
+# Add new columns to the ShapeFile containing the new calculated index
 census_inhabitants['greenSpaceIndex'] = np.array(greenSpaceIndex)
 census_inhabitants['greenSpaceDensity'] = np.array(greenSpaceDensity)
 census_inhabitants['greenSpaceBuiltSpaceRatio'] = np.array(greenSpaceBuiltSpaceRatio)
 
-census_inhabitants.to_excel('out.xls')
+# Save results into a new ShapeFile
+census_inhabitants = geopandas.GeoDataFrame(census_inhabitants, geometry='geometry')
+census_inhabitants.to_file('out/indexes1.shp', driver='ESRI Shapefile')
 
 print('fin')
--- calc_greenSpacesProximityIndex.py
+++ calc_greenSpacesProximityIndex.py
@@ -1,42 +1,44 @@
 import pandas as pd
+import geopandas
 from read_shapeFile import loadShapeFile
-import numpy as np
-import time
 
-columns = pd.read_excel('data/secciones_censales/2801_CensoMadrid2019.xls').loc[5]
-census = pd.read_excel('data/secciones_censales/2801_CensoMadrid2019.xls', names = columns, header = 8, index_col=0)
-census.index = census.index.str.strip()
-census = census.groupby(census.index).sum()
-
-seccionesCensales_shp = loadShapeFile('data/secciones_censales/SECC_CE_20200101_MADRID.shp')
-census_inhabitants = census.rename({'Total': 'Inhabitants'}, axis=1)['Inhabitants']
-census_inhabitants = seccionesCensales_shp.join(census_inhabitants, on='CUSEC')
+# Load ShapeFiles of green areas
 urbanGardens_shp = loadShapeFile('data/Carto_1000/11_HUERTO_URBANO_P.shp')
 gardenAreas_shp = loadShapeFile('data/Carto_1000/11_ZONA_AJARDINADA_P.shp')
 gardenInPatios_shp = loadShapeFile('data/Carto_1000/11_ZONA_AJARDINADA_SOBRE_PATIO_P.shp')
 
+# Crete arrays for the different calculated index
 urbanGardens_index = []
 gardenAreas_index = []
 gardenInPatios_index = []
+
+# Minimum distance to which other green areas are checked
+min_prox_dist = 200
 
 print('Starting Urban Gardens')
 for garden in urbanGardens_shp['geometry']:
     distance = 10000
 
     for urbanGarden_polygon in urbanGardens_shp['geometry']:
-        if (garden.distance(urbanGarden_polygon) < distance) and (garden.distance(urbanGarden_polygon) != 0):
+        if (garden.distance(urbanGarden_polygon) < distance) and (garden.distance(urbanGarden_polygon) <= min_prox_dist):
             distance = garden.distance(urbanGarden_polygon)
 
     for idx, gardenArea_polygon in enumerate(gardenAreas_shp['geometry']):
         if idx != 72:
-            if (garden.distance(gardenArea_polygon) < distance) and (garden.distance(gardenArea_polygon) != 0):
+            if (garden.distance(gardenArea_polygon) < distance) and (garden.distance(gardenArea_polygon) <= min_prox_dist):
                 distance = garden.distance(gardenArea_polygon)
 
     for gardenInPatio_polygon in gardenInPatios_shp['geometry']:
-        if (garden.distance(gardenInPatio_polygon) < distance) and (garden.distance(gardenInPatio_polygon) != 0):
+        if (garden.distance(gardenInPatio_polygon) < distance) and (garden.distance(gardenInPatio_polygon) <= min_prox_dist):
             distance = garden.distance(gardenInPatio_polygon)
 
-    prox = garden.area/(distance * distance)
+    if distance == 10000:
+        prox = 0
+    else:
+        try:
+            prox = garden.area/(distance * distance)
+        except ZeroDivisionError:
+            prox = garden.area
     urbanGardens_index.append(prox)
 
 print('Starting Garden Areas')
@@ -44,19 +46,25 @@ for garden in gardenAreas_shp['geometry']:
     distance = 10000
 
     for urbanGarden_polygon in urbanGardens_shp['geometry']:
-        if (garden.distance(urbanGarden_polygon) < distance) and (garden.distance(urbanGarden_polygon) != 0):
+        if (garden.distance(urbanGarden_polygon) < distance) and (garden.distance(urbanGarden_polygon) <= min_prox_dist):
             distance = garden.distance(urbanGarden_polygon)
 
     for idx, gardenArea_polygon in enumerate(gardenAreas_shp['geometry']):
         if idx != 72:
-            if (garden.distance(gardenArea_polygon) < distance) and (garden.distance(gardenArea_polygon) != 0):
+            if (garden.distance(gardenArea_polygon) < distance) and (garden.distance(gardenArea_polygon) <= min_prox_dist):
                 distance = garden.distance(gardenArea_polygon)
 
     for gardenInPatio_polygon in gardenInPatios_shp['geometry']:
-        if (garden.distance(gardenInPatio_polygon) < distance) and (garden.distance(gardenInPatio_polygon) != 0):
+        if (garden.distance(gardenInPatio_polygon) < distance) and (garden.distance(gardenInPatio_polygon) <= min_prox_dist):
             distance = garden.distance(gardenInPatio_polygon)
 
-    prox = garden.area/(distance * distance)
+    if distance == 10000:
+        prox = 0
+    else:
+        try:
+            prox = garden.area/(distance * distance)
+        except ZeroDivisionError:
+            prox = garden.area
     gardenAreas_index.append(prox)
 
 print('Starting Garden in Patios')
@@ -64,19 +72,36 @@ for garden in gardenInPatios_shp['geometry']:
     distance = 10000
 
     for urbanGarden_polygon in urbanGardens_shp['geometry']:
-        if (garden.distance(urbanGarden_polygon) < distance) and (garden.distance(urbanGarden_polygon) != 0):
+        if (garden.distance(urbanGarden_polygon) < distance) and (garden.distance(urbanGarden_polygon) <= min_prox_dist):
             distance = garden.distance(urbanGarden_polygon)
 
     for idx, gardenArea_polygon in enumerate(gardenAreas_shp['geometry']):
         if idx != 72:
-            if (garden.distance(gardenArea_polygon) < distance) and (garden.distance(gardenArea_polygon) != 0):
+            if (garden.distance(gardenArea_polygon) < distance) and (garden.distance(gardenArea_polygon) <= min_prox_dist):
                 distance = garden.distance(gardenArea_polygon)
 
     for gardenInPatio_polygon in gardenInPatios_shp['geometry']:
-        if (garden.distance(gardenInPatio_polygon) < distance) and (garden.distance(gardenInPatio_polygon) != 0):
+        if (garden.distance(gardenInPatio_polygon) < distance) and (garden.distance(gardenInPatio_polygon) <= min_prox_dist):
             distance = garden.distance(gardenInPatio_polygon)
 
-    prox = garden.area / (distance * distance)
-    urbanGardens_index.append(prox)
+    if distance == 10000:
+        prox = 0
+    else:
+        try:
+            prox = garden.area/(distance * distance)
+        except ZeroDivisionError:
+            prox = garden.area
+    gardenInPatios_index.append(prox)
+
+print('Saving files')
+
+# Convert files to geoPandas GeoDataFrame and save them
+urbanGardens_shp = geopandas.GeoDataFrame(urbanGardens_shp, geometry='geometry')
+gardenAreas_shp = geopandas.GeoDataFrame(gardenAreas_shp, geometry='geometry')
+gardenInPatios_shp = geopandas.GeoDataFrame(gardenInPatios_shp, geometry='geometry')
+
+urbanGardens_shp.to_file('out/urbanGardens.shp', driver='ESRI Shapefile')
+gardenAreas_shp.to_file('out/gardenAreas.shp', driver='ESRI Shapefile')
+gardenInPatios_shp.to_file('out/gardenInPatios.shp', driver='ESRI Shapefile')
 
 print('Done')
